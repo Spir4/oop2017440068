94ef269cfd0abd1c4a9460a116efbf7f95ee637a
2017440068 <ieppersbj@naver.com>
2018-04-03 17:32:35 +0900

    20180403

---
+++ 20180403 a/src/String.java
@@ -0,0 +1,17 @@
+
+public class String {
+
+	public String(java.lang.String string) {
+
+	}
+
+	public static void main(java.lang.String[] args) {
+
+		String str1= new String("Hello");
+		String str2= new String("Hello");
+		str1.equals(str2);
+		boolean a= str1==str2;
+		System.out.println(a);
+	}
+
+}
